a0387d1da40ad75346f43e56479861f2c6e803c6
ibrahim <ibrahim.haouari@breakpoint-technology.fr>
2020-12-25 13:23:35 +0100

    init index file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,46 @@
 import React from 'react'
-import styles from './styles.module.css'
+import PageNav from './PageNav'
+import PageScroller from './PageScroller'
+import PageIndicator from './PageIndicator'
+import Page from './Page'
+import PropTypes from 'prop-types'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const FullPage = (props) => {
+  const { indicator, nav, children } = props
+  return (
+    <PageScroller style={{ display: 'flex', flexDirection: 'column' }}>
+      {nav && <PageNav />}
+      {indicator && <PageIndicator />}
+
+      {React.Children.toArray(children).map((child, id) => {
+        return (
+          <Page
+            key={`react-full-page-${id}`}
+            style={{
+              backgroundColor: 'blue',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}
+            navTitle='Num 1'
+          >
+            {child}
+          </Page>
+        )
+      })}
+    </PageScroller>
+  )
 }
+
+FullPage.propTypes = {
+  children: PropTypes.node.isRequired,
+  indicator: PropTypes.bool,
+  nav: PropTypes.bool
+}
+
+FullPage.defaultProps = {
+  indicator: false,
+  nav: false
+}
+
+export default FullPage
