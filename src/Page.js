d4f7ab927ea76309a7e76c5e8a14ce6644572d43
ibrahim <ibrahim.haouari@breakpoint-technology.fr>
2020-12-25 13:21:49 +0100

    add page component

---
+++ src/Page.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Page = (props) => {
+  const { style, children, ...otherProps } = { ...props }
+  const { height, width, ...cleanedStyle } = { ...style }
+
+  const pageStyle = {
+    height: '100vh',
+    width: '100vw',
+    ...cleanedStyle
+  }
+
+  return (
+    <div style={pageStyle} {...otherProps}>
+      {children}
+    </div>
+  )
+}
+export default Page
