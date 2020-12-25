7874ea435b1b6d84cc02ee2b3daea65670a71075
ibrahim <ibrahim.haouari@breakpoint-technology.fr>
2020-12-25 13:21:31 +0100

    add page nav component

---
+++ src/PageNav.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const PageNav = (props) => {
+  const { goToPage, navButtons } = props
+  const renderButtons = () => {
+    const buttons = []
+    navButtons.map((buttonInfo, i) => {
+      buttons.push(
+        <div
+          className='page-nav-button'
+          onClick={() => goToPage(buttonInfo.index)}
+          key={i}
+        >
+          {buttonInfo.title}
+        </div>
+      )
+    })
+
+    return buttons
+  }
+  return <div className='page-nav'>{renderButtons()}</div>
+}
+
+PageNav.propTypes = {
+  navButtons: PropTypes.array,
+  goToPage: PropTypes.func
+}
+
+PageNav.defaultProps = {
+  navButtons: [],
+  goToPage: () => {}
+}
+
+export default PageNav
