1200d146d4e6ab350197a61f7729726b9fb88ddb
Ibrahim Haouari <ibrahim.haouari@gmail.com>
2021-11-03 11:59:55 +0100

    add react image

--- example/src/App.js
+++ example/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react'
 
 import MyComponent from 'react-full-page-scroller'
-import 'react-full-page-scroller/dist/index.css'
+import logo from './1200px-React-icon.svg.png'
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 
 const App = () => {
   const nav = [
     {
       id: 1,
-      title: 'title 1',
+      title: 'Title 1',
       className: 'page-nav-button',
       style: { width: '100%' }
     },
     {
       id: 2,
-      title: 'title 2',
+      title: 'Title 2',
       className: 'page-nav-button',
       style: { width: '100%' }
     },
     {
       id: 3,
-      title: 'title 3',
+      title: 'Title 3',
       className: 'page-nav-button',
       style: { width: '100%' }
     }
@@ -34,6 +36,29 @@ const App = () => {
   }
   const indicatorStyleActive = { width: '20px' }
 
+  const codeString ="import React, { Component } from 'react'\n" +
+    "\n" +
+    "import MyComponent from 'react-full-page-scroller'\n" +
+    "import 'react-full-page-scroller/dist/index.css'\n" +
+    "\n" +
+    "class Example extends Component {\n" +
+    "  render() {\n" +
+    "    return (\n" +
+    "      <MyComponent>\n" +
+    "        <div>\n" +
+    "          <h1>1</h1>\n" +
+    "        </div>\n" +
+    "        <div>\n" +
+    "          <h1>2</h1>\n" +
+    "        </div>\n" +
+    "        <div>\n" +
+    "          <h1>3</h1>\n" +
+    "        </div>\n" +
+    "      </MyComponent>\n" +
+    "    )\n" +
+    "  }\n" +
+    "}"
+
   return (
     <MyComponent
       pageNav={nav}
@@ -43,22 +68,29 @@ const App = () => {
       <div
         className='bg-blue'
         style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center'
+          display: 'block',
+          textAlign: 'center',
+          margin: 'auto'
         }}
       >
-        <h1 className='page-number'>1</h1>
+        <img src={logo} alt="Logo" height="50%" />
+        <h1 className='page-number'>React Full Page Scroller</h1>
+        <button style={{height:'30px', cursor: 'pointer'}} onClick={() => window.location.href="https://github.com/ibrahim-ih/react-full-page-scroller"}>View on Github</button>
       </div>
       <div
         className='bg-green'
         style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center'
+          display: 'block',
+          textAlign: 'center',
+          margin: 'auto'
         }}
       >
-        <h1 className='page-number'>2</h1>
+        <h1 className='page-number'>USAGE</h1>
+        <div style={{width: "50%", margin: "0 auto"}}>
+          <SyntaxHighlighter language="javascript" style={dark}>
+            {codeString}
+          </SyntaxHighlighter>
+        </div>
       </div>
       <div
         className='bg-red'
@@ -68,7 +100,7 @@ const App = () => {
           justifyContent: 'center'
         }}
       >
-        <h1 className='page-number'>3</h1>
+        <h1 className='page-number'>Section 3</h1>
       </div>
     </MyComponent>
   )
